889b24308738bf359cbab2a89d9e196672c6f19c
Felipe <felipe.lipiss@gmail.com>
2016-03-08 09:36:47 +0100

    Update sp_buildCrossTable.sql

--- sp_buildCrossTable.sql
+++ sp_buildCrossTable.sql
@@ -1,1 +1,59 @@
 
+DROP PROCEDURE IF EXISTS 
+stats_build_crosstable;
+/*
+The characters for the stored procedure are special for dbVisualizer.
+This an example of the usual sintax, changing the delimiter.
+    DELIMITER //
+    CREATE PROCEDURE GetOfficeByCountry(IN countryName VARCHAR(255))
+     BEGIN
+     SELECT * 
+     FROM offices
+     WHERE country = countryName;
+     END //
+    DELIMITER ;
+*/
+--/
+CREATE PROCEDURE 
+stats_build_crosstable
+(
+IN rowField VARCHAR(1000),
+IN toColumnValueField VARCHAR(1000),
+IN srcTable VARCHAR(2000),
+IN ifValueReturn VARCHAR(1000),
+IN ifResultOperation VARCHAR(1000),
+IN calcSyntax VARCHAR(100),
+IN returnField VARCHAR(100),
+IN filter_query VARCHAR(400),
+IN groupByFields VARCHAR(1000),
+IN orderByFields VARCHAR(1000)
+)
+BEGIN
+	  SET SESSION group_concat_max_len = 1000000;
+	  SET @sql = NULL;
+	  SET @SUBSQL= CONCAT("SELECT GROUP_CONCAT(SUM_FIELD) into @sql from (SELECT DISTINCT CONCAT('",ifResultOperation,"(IF(", toColumnValueField," = ''',", toColumnValueField, ",''', ",ifValueReturn,",0)) AS ''',", toColumnValueField, ",'''') SUM_FIELD FROM ",srcTable,filter_query," ORDER BY ",toColumnValueField,") A;");
+	  /*SELECT @SUBSQL;*/
+	  PREPARE stmt FROM @SUBSQL;
+	  EXECUTE stmt;
+	  DEALLOCATE PREPARE stmt;
+	  /*SELECT @sql;*/
+	  SET @sql = CONCAT('SELECT ',rowField, @sql,',', calcSyntax,'(',returnField,') as TOTAL ', 'FROM ',srcTable,filter_query,groupByFields,orderByFields);  
+	  /*SELECT @sql;*/
+	  PREPARE stmt FROM @sql; 
+	  EXECUTE stmt;
+	  DEALLOCATE PREPARE stmt;
+END;
+/
+
+
+call stats__build_crosstable(
+'RncName,UARFCNDL,',/*rowField*/
+'Causes',/*toColumnValueField*/
+'fp_report_el_root_causes',/*srcTable*/
+'1',/*ifValueReturn*/
+'SUM',/*ifResultOperation*/
+'count',/*calcSyntax*/
+'*',/*returnField*/
+'',/*filter_query*/
+' GROUP BY RncName,UARFCNDL',/*groupByFields*/
+''/*orderByFields*/);
